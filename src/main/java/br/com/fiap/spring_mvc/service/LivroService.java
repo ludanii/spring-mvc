ec531454ab865cddbb7d168714f92caf4ab96761
ludanii <ludanielle2005@hotmail.com>
2025-08-12 22:33:57 -0300

    Aula 02 - 12/08/2025

---
+++ src/main/java/br/com/fiap/spring_mvc/service/LivroService.java
@@ -0,0 +1,36 @@
+package br.com.fiap.spring_mvc.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.fiap.spring_mvc.entity.Livro;
+import br.com.fiap.spring_mvc.repository.LivroRepository;
+
+@Service
+public class LivroService {
+  private final LivroRepository livroRepository;
+
+  @Autowired
+  public LivroService(LivroRepository livroRepository){
+    this.livroRepository = livroRepository;
+  }
+  // CRUD
+  public Livro createdlivro(Livro livro){
+    return livroRepository.save(livro);
+  }
+  public Livro readLivro(Long id){
+    return livroRepository.findById(id).orElse(null);
+  }
+  public List<Livro> readLivros(){
+    return livroRepository.findAll();
+  }
+  public Livro updateLivro(Livro livro){
+    return livroRepository.save(livro);
+  }
+  public void deleteLivro(Long id){
+    livroRepository.deleteById(id);
+  }
+
+}
